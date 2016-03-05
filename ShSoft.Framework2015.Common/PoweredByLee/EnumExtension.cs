675a1074b5d20a11434dff0913b2005375428961
Lee <lishilei0523@163.com>
2016-03-05 16:43:41 +0800

    增加Common类库中获取枚举名称字典方法。

--- ShSoft.Framework2015.Common/PoweredByLee/EnumExtension.cs
+++ ShSoft.Framework2015.Common/PoweredByLee/EnumExtension.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Reflection;
 
 namespace ShSoft.Framework2015.Common.PoweredByLee
@@ -9,6 +11,7 @@ namespace ShSoft.Framework2015.Common.PoweredByLee
     /// </summary>
     public static class EnumExtension
     {
+        #region # 获取枚举成员名称 —— static string GetEnumMember(this Enum @enum)
         /// <summary>
         /// 获取枚举成员名称
         /// </summary>
@@ -23,5 +26,38 @@ namespace ShSoft.Framework2015.Common.PoweredByLee
                 : string.IsNullOrEmpty(enumMember.Description) ? @enum.ToString() :
                 enumMember.Description;
         }
+        #endregion
+
+        #region # 获取枚举成员名称字典 —— static IDictionary<string, string> GetEnumMembers(...
+        /// <summary>
+        /// 获取枚举成员名称字典
+        /// </summary>
+        /// <param name="enumType">枚举类型</param>
+        /// <returns>枚举成员名称字典</returns>
+        public static IDictionary<string, string> GetEnumMembers(this Type enumType)
+        {
+            #region # 验证参数
+
+            if (!enumType.IsSubclassOf(typeof(Enum)))
+            {
+                throw new ArgumentOutOfRangeException(string.Format("类型\"{0}\"不是枚举类型！", enumType.Name));
+            }
+
+            #endregion
+
+            FieldInfo[] fields = enumType.GetFields();
+
+            IDictionary<string, string> dictionaries = new Dictionary<string, string>();
+
+            foreach (FieldInfo field in fields.Where(x => x.Name != "value__"))
+            {
+                DescriptionAttribute enumMember = field.GetCustomAttribute<DescriptionAttribute>();
+
+                dictionaries.Add(field.Name, enumMember == null ? field.Name : string.IsNullOrEmpty(enumMember.Description) ? field.Name : enumMember.Description);
+            }
+
+            return dictionaries;
+        }
+        #endregion
     }
 }
