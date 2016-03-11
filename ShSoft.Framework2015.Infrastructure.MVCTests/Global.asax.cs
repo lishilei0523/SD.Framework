ed7c4964be273973328dce76b5a33b6bca0270bf
Lee <lishilei0523@163.com>
2016-03-11 15:20:55 +0800

    -

--- ShSoft.Framework2015.Infrastructure.MVCTests/Global.asax.cs
+++ ShSoft.Framework2015.Infrastructure.MVCTests/Global.asax.cs
@@ -1,15 +1,13 @@
 ﻿using System.Web;
-using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 using ShSoft.Framework2015.Infrastructure.MVC;
 
 namespace ShSoft.Framework2015.Infrastructure.MVCTests
 {
-    // 注意: 有关启用 IIS6 或 IIS7 经典模式的说明，
-    // 请访问 http://go.microsoft.com/?LinkId=9394801
-
+    /// <summary>
+    /// 全局应用程序类
+    /// </summary>
     public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
