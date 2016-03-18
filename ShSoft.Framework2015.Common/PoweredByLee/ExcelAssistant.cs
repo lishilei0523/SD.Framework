4a7ebce04dada3c09e4f643284e4b9db821d1828
Lee <lishilei0523@163.com>
2016-03-18 18:03:06 +0800

    -

--- ShSoft.Framework2015.Common/PoweredByLee/ExcelAssistant.cs
+++ ShSoft.Framework2015.Common/PoweredByLee/ExcelAssistant.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices.ComTypes;
 using NPOI.HSSF.UserModel;
 using NPOI.SS.UserModel;
 
