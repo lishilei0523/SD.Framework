15f4d24634c82a8200f0ca6a1a7009df13415e9a
Lee <lishilei0523@163.com>
2016-03-18 14:00:06 +0800

    Infrastructure.MVC内部实现调整

--- ShSoft.Framework2015.Infrastructure.MVC/Base/BaseMvcApplication.cs
+++ ShSoft.Framework2015.Infrastructure.MVC/Base/BaseMvcApplication.cs
@@ -1,30 +1,20 @@
 ﻿using System;
-using ShSoft.Framework2015.Infrastructure.Web.Base;
+using System.Web;
 
 namespace ShSoft.Framework2015.Infrastructure.MVC
 {
     /// <summary>
     /// MVC全局应用程序类基类
     /// </summary>
-    public abstract class BaseMvcApplication : BaseHttpApplication
+    public abstract class BaseMvcApplication : HttpApplication
     {
         /// <summary>
         /// 应用程序开始事件
         /// </summary>
-        protected override void Application_Start(object sender, EventArgs e)
+        protected virtual void Application_Start(object sender, EventArgs e)
         {
-            base.Application_Start(sender, e);
-
             //注册IOC配置
             IocConfig.Register();
-        }
-
-        /// <summary>
-        /// 请求结束事件
-        /// </summary>
-        protected virtual void Application_EndRequest(object sender, EventArgs e)
-        {
-
         }
     }
 }
