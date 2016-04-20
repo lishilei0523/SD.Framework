51853eddf24d530f7c05b8657c95ca2afd180678
Lee <lishilei0523@163.com>
2016-04-20 14:30:37 +0800

    将MVC依赖注入改造为IHttpModule形式

---
+++ ShSoft.Framework2016.Infrastructure.MVC.IOC/IocHttpModule.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using SD.IOC.Integration.MVC;
+
+namespace ShSoft.Framework2016.Infrastructure.MVC.IOC
+{
+    /// <summary>
+    /// 依赖注入HttpModule
+    /// </summary>
+    internal class IocHttpModule : IHttpModule
+    {
+        /// <summary>
+        /// 初始化，
+        /// 相当于Application_Start事件
+        /// </summary>
+        /// <param name="context">应用程序上下文</param>
+        public void Init(HttpApplication context)
+        {
+            DependencyResolver.SetResolver(new MvcDependencyResolver());
+        }
+
+        /// <summary>
+        /// 释放资源，
+        /// 相当于Application_End事件
+        /// </summary>
+        public void Dispose() { }
+    }
+}
