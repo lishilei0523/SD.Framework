41ce95f4748419a3e230a032a099cce6d09b6390
Lee <lishilei0523@163.com>
2016-07-03 14:21:53 +0800

    添加生成注释XML

--- ShSoft.Framework2016.Infrastructure.WCF/Properties/AssemblyInfo.cs
+++ ShSoft.Framework2016.Infrastructure.WCF/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using ShSoft.Framework2016.Infrastructure.WCF;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("ShSoft.Framework2016.Infrastructure.WCF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("基础设施 - WCF基础")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("山河软件")]
 [assembly: AssemblyProduct("ShSoft.Framework2016.Infrastructure.WCF")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright © ShSoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using ShSoft.Framework2016.Infrastructure.WCF;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
