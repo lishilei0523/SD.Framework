5df9db72bd325770da1ffa34c719b5dad82009ec
Lee <lishilei0523@163.com>
2016-07-19 21:59:10 +0800

    提交遗漏文件

---
+++ ShSoft.Infrastructure.WCFTests/Stubs/StubDbCleaner.cs
@@ -0,0 +1,18 @@
+﻿using ShSoft.Infrastructure.RepositoryBase;
+
+namespace ShSoft.Infrastructure.WCFTests.Stubs
+{
+    /// <summary>
+    /// 数据库清理者桩
+    /// </summary>
+    public class StubDbCleaner : IDbCleaner
+    {
+        /// <summary>
+        /// 清理
+        /// </summary>
+        public void Clean()
+        {
+
+        }
+    }
+}
---
+++ ShSoft.Infrastructure/CommandBase/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using ShSoft.Infrastructure.MessageBase;
+
+namespace ShSoft.Infrastructure.CommandBase
+{
+    /// <summary>
+    /// 命令基接口
+    /// </summary>
+    public interface ICommand : IMessage
+    {
+
+    }
+}
