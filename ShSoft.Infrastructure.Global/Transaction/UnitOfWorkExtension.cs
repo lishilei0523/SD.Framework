d89516eeac25e386d7b32ce80ae05aa1377faa9f
Lee <lishilei0523@163.com>
2016-07-25 11:44:09 +0800

    增加工作单元集联合提交扩展方法

--- ShSoft.Infrastructure.Global/Transaction/UnitOfWorkExtension.cs
+++ ShSoft.Infrastructure.Global/Transaction/UnitOfWorkExtension.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Transactions;
 using ShSoft.Infrastructure.EventBase.Mediator;
 using ShSoft.Infrastructure.RepositoryBase;
@@ -56,6 +59,63 @@ namespace ShSoft.Infrastructure.Global.Transaction
         public static async Task UnitedCommitAsync(this IUnitOfWork unitOfWork)
         {
             await Task.Run(() => UnitedCommit(unitOfWork));
+        }
+
+        /// <summary>
+        /// UnitOfWork集联合提交（包含处理领域事件）扩展方法
+        /// </summary>
+        /// <param name="unitOfWorks">工作单元实例集</param>
+        public static void UnitedCommit(this IEnumerable<IUnitOfWork> unitOfWorks)
+        {
+            #region # 验证参数
+
+            if (unitOfWorks == null)
+            {
+                throw new ArgumentNullException("unitOfWorks", "工作单元实例集不可为空！");
+            }
+
+            #endregion
+
+            unitOfWorks = unitOfWorks.ToArray();
+
+            try
+            {
+                //开启事务
+                using (TransactionScope scope = new TransactionScope())
+                {
+                    foreach (IUnitOfWork unitOfWork in unitOfWorks)
+                    {
+                        //提交工作单元
+                        unitOfWork.Commit();
+                    }
+
+                    //处理领域事件
+                    EventMediator.HandleUncompletedEvents();
+
+                    //事务完成
+                    scope.Complete();
+                }
+            }
+            catch
+            {
+                //不参与事务
+                using (TransactionScope scope = new TransactionScope(TransactionScopeOption.Suppress))
+                {
+                    //回滚工作单元
+                    foreach (IUnitOfWork unitOfWork in unitOfWorks)
+                    {
+                        //提交工作单元
+                        unitOfWork.Commit();
+                    }
+
+                    //清空未处理的领域事件
+                    EventMediator.ClearUncompletedEvents();
+
+                    //事务完成 
+                    scope.Complete();
+                }
+                throw;
+            }
         }
     }
 }
