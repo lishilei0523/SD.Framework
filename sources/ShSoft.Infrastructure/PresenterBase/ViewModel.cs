a7e84e237e1d3c485e0587cd073256dd71f26347
Lee <lishilei0523@163.com>
2016-11-30 10:40:53 +0800

    增加视图模型基类

---
+++ sources/ShSoft.Infrastructure/PresenterBase/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ShSoft.Infrastructure.PresenterBase
+{
+    /// <summary>
+    /// 视图模型基类
+    /// </summary>
+    public abstract class ViewModel
+    {
+        /// <summary>
+        /// 标识
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
