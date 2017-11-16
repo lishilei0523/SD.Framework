3aacf92d2f4a4ff4e160c057a6114f31ae5eabe7
Lee <lishilei0523@163.com>
2017-11-16 16:57:32 +0800

    增加登录菜单通用模型

---
+++ sources/SD.Infrastructure/Constants/LoginMenuInfo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace SD.Infrastructure.Constants
+{
+    /// <summary>
+    /// 登录菜单树
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class LoginMenuInfo
+    {
+        /// <summary>
+        /// 无参构造器
+        /// </summary>
+        public LoginMenuInfo()
+        {
+            this.SubMenuInfos = new List<LoginMenuInfo>();
+        }
+
+        /// <summary>
+        /// 标识Id
+        /// </summary>
+        [DataMember]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// 上级菜单Id
+        /// </summary>
+        [DataMember]
+        public Guid? ParentId { get; set; }
+
+        /// <summary>
+        /// 菜单名称
+        /// </summary>
+        [DataMember]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 菜单排序
+        /// </summary>
+        [DataMember]
+        public int Sort { get; set; }
+
+        /// <summary>
+        /// 菜单图标
+        /// </summary>
+        [DataMember]
+        public string Icon { get; set; }
+
+        /// <summary>
+        /// 链接
+        /// </summary>
+        [DataMember]
+        public string Url { get; set; }
+
+        /// <summary>
+        /// 路径
+        /// </summary>
+        [DataMember]
+        public string Path { get; set; }
+
+        /// <summary>
+        /// 子菜单集
+        /// </summary>
+        [DataMember]
+        public IList<LoginMenuInfo> SubMenuInfos { get; set; }
+    }
+}
