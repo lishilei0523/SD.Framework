f7e07a2d9714e9318c47153a4c267f547a67c9e8
Lee <lishilei0523@163.com>
2018-02-12 09:43:01 +0800

    信息系统增加应用程序类型

--- sources/SD.Infrastructure/MemberShip/LoginSystemInfo.cs
+++ sources/SD.Infrastructure/MemberShip/LoginSystemInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SD.Infrastructure.Constants;
+using System;
 using System.Runtime.Serialization;
 
 namespace SD.Infrastructure.MemberShip
@@ -20,12 +21,14 @@ namespace SD.Infrastructure.MemberShip
         /// </summary>
         /// <param name="systemNo">信息系统编号</param>
         /// <param name="systemName">信息系统名称</param>
+        /// <param name="applicationType">应用程序类型</param>
         /// <param name="index">首页</param>
-        public LoginSystemInfo(string systemNo, string systemName, string index)
+        public LoginSystemInfo(string systemNo, string systemName, ApplicationType applicationType, string index)
             : this()
         {
             this.SystemNo = systemNo;
             this.SystemName = systemName;
+            this.ApplicationType = applicationType;
             this.Index = index;
         }
 
@@ -41,6 +44,12 @@ namespace SD.Infrastructure.MemberShip
         /// </summary>
         [DataMember]
         public string SystemName { get; set; }
+
+        /// <summary>
+        /// 应用程序类型
+        /// </summary>
+        [DataMember]
+        public ApplicationType ApplicationType { get; set; }
 
         /// <summary>
         /// 首页
