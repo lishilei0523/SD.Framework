37a1cfd56df8985683feb18df8c7777a6550e198
Lee <lishilei0523@163.com>
2020-08-17 16:58:53 +0800

    SignalR客户端基础完善；

---
+++ src/SD.Infrastructure.SignalR.Client/Extensions/HubConnectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using SD.Infrastructure.Constants;
+using System;
+
+namespace SD.Infrastructure.SignalR.Client.Extensions
+{
+    /// <summary>
+    /// Hub连接扩展方法
+    /// </summary>
+    public static class HubConnectionExtension
+    {
+        /// <summary>
+        /// 注册公钥
+        /// </summary>
+        /// <param name="connection">Hub连接</param>
+        /// <param name="publicKey">公钥</param>
+        public static void RegisterPublicKey(this HubConnection connection, Guid publicKey)
+        {
+            connection.Headers.Add(SessionKey.CurrentPublishKey, publicKey.ToString());
+        }
+    }
+}
