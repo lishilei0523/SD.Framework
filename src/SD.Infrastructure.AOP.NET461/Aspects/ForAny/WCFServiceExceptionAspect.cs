b0d68550aa56734ec58bdb536d309ecc53e19bba
Lee <lishilei0523@163.com>
2021-03-30 11:15:10 +0800

    AOP .NET Framework 4.6.1增加WCF服务异常切面；

---
+++ src/SD.Infrastructure.AOP.NET461/Aspects/ForAny/WCFServiceExceptionAspect.cs
@@ -0,0 +1,29 @@
+﻿using ArxOne.MrAdvice.Advice;
+using SD.AOP.Core.Aspects.ForAny;
+using SD.Toolkits.Json;
+using System;
+using System.ServiceModel;
+
+namespace SD.Infrastructure.AOP.Aspects.ForAny
+{
+    /// <summary>
+    /// WCF服务层异常AOP特性类
+    /// </summary>
+    [Serializable]
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    public sealed class WCFServiceExceptionAspect : ExceptionAspect
+    {
+        /// <summary>
+        /// 发生异常事件
+        /// </summary>
+        /// <param name="context">方法元数据</param>
+        /// <param name="exception">异常实例</param>
+        protected override void OnException(MethodAdviceContext context, Exception exception)
+        {
+            base.OnException(context, exception);
+
+            //抛出异常
+            throw new FaultException(base._exceptionMessage.ToJson());
+        }
+    }
+}
