9006d3274dd03702c117325e8b8a9df59dbdc546
Lee <lishilei0523@163.com>
2021-04-24 11:16:33 +0800

    -

--- src/SD.Infrastructure.Repository.MongoDB.Tests/Properties/AssemblyInfo.cs
+++ src/SD.Infrastructure.Repository.MongoDB.Tests/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下特性集 
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("SD.Infrastructure.Repository.MongoDBTests")]
+[assembly: AssemblyTitle("SD.Infrastructure.Repository.MongoDB.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("SD.Infrastructure.Repository.MongoDBTests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCompany("SD")]
+[assembly: AssemblyProduct("SD.Infrastructure.Repository.MongoDB.Tests")]
+[assembly: AssemblyCopyright("Copyright © SD 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly:  AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
