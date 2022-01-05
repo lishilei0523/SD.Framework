8a2ad6adbcf5305d51e5fbacf109f496124aeda2
Lee <lishilei0523@163.com>
2022-01-05 18:41:33 +0800

    补充模型类；

---
+++ src/SD.Infrastructure.Xamarin/Models/MenuItemGroup.cs
@@ -0,0 +1,27 @@
+﻿using SD.Infrastructure.Membership;
+using System.Collections.Generic;
+
+namespace SD.Infrastructure.Xamarin.Models
+{
+    /// <summary>
+    /// 菜单项组
+    /// </summary>
+    public class MenuItemGroup : List<LoginMenuInfo>
+    {
+        /// <summary>
+        /// 菜单项组名称
+        /// </summary>
+        public string GroupName { get; private set; }
+
+        /// <summary>
+        /// 创建菜单项组构造器
+        /// </summary>
+        /// <param name="menuGroupName">菜单项组名称</param>
+        /// <param name="menuItems">菜单项列表</param>
+        public MenuItemGroup(string menuGroupName, IEnumerable<LoginMenuInfo> menuItems)
+            : base(menuItems)
+        {
+            this.GroupName = menuGroupName;
+        }
+    }
+}
