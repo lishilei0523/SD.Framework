6e83ca4e079965fa18b101fea88c4eef35c1a28f
Lee <lishilei0523@163.com>
2022-01-23 11:37:27 +0800

    WPF Caliburn 增加通知；

--- src/SD.Infrastructure.WPF.Caliburn/Base/ScreenBase.cs
+++ src/SD.Infrastructure.WPF.Caliburn/Base/ScreenBase.cs
@@ -1,20 +1,100 @@
 ﻿using Caliburn.Micro;
 using SD.Infrastructure.WPF.Caliburn.Aspects;
+using SD.Infrastructure.WPF.Caliburn.Models;
 using SD.Infrastructure.WPF.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using ToastNotifications;
+using ToastNotifications.Core;
+using ToastNotifications.Lifetime;
+using ToastNotifications.Lifetime.Clear;
+using ToastNotifications.Messages;
+using ToastNotifications.Position;
 
 namespace SD.Infrastructure.WPF.Caliburn.Base
 {
     /// <summary>
     /// Screen基类
     /// </summary>
-    public class ScreenBase : Screen, IBusy
+    public abstract class ScreenBase : Screen, IBusy
     {
+        #region # 字段及构造器
+
+        /// <summary>
+        /// 通知
+        /// </summary>
+        private readonly Notifier _notifier;
+
+        /// <summary>
+        /// 无参构造器
+        /// </summary>
+        protected ScreenBase()
+        {
+            //初始化通知
+            this._notifier = new Notifier(options =>
+            {
+                options.PositionProvider = new WindowPositionProvider(Application.Current.MainWindow, this.NotifierOptions.Location, this.NotifierOptions.MarginX, this.NotifierOptions.MarginY);
+                options.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(TimeSpan.FromSeconds(this.NotifierOptions.Lifetime), MaximumNotificationCount.FromCount(this.NotifierOptions.MaxCount));
+                options.DisplayOptions.TopMost = false;
+                options.Dispatcher = Dispatcher.CurrentDispatcher;
+            });
+            this._notifier.ClearMessages(new ClearAll());
+        }
+
+        #endregion
+
+        #region # 属性
+
+        #region 是否繁忙 —— bool IsBusy
         /// <summary>
         /// 是否繁忙
         /// </summary>
         [DependencyProperty]
         public bool IsBusy { get; set; }
+        #endregion
 
+        #region 只读属性 - 通知设置 —— virtual NotifierOptions NotifierOptions
+        /// <summary>
+        /// 只读属性 - 通知设置
+        /// </summary>
+        public virtual NotifierOptions NotifierOptions
+        {
+            get => new NotifierOptions();
+        }
+        #endregion
+
+        #endregion
+
+        #region # 方法
+
+        #region 失去活动 —— override void OnDeactivate(bool close)
+        /// <summary>
+        /// 失去活动
+        /// </summary>
+#if NET40 || NET45
+        protected override void OnDeactivate(bool close)
+#endif
+#if NET461_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+        protected override Task OnDeactivateAsync(bool close, CancellationToken cancellationToken)
+#endif
+        {
+            if (close)
+            {
+                this._notifier?.Dispose();
+            }
+#if NET40 || NET45
+            base.OnDeactivate(close);
+#endif
+#if NET461_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+            return base.OnDeactivateAsync(close, cancellationToken);
+#endif
+        }
+        #endregion
+
+        #region 挂起繁忙状态 —— void Busy()
         /// <summary>
         /// 挂起繁忙状态
         /// </summary>
@@ -22,7 +102,9 @@ namespace SD.Infrastructure.WPF.Caliburn.Base
         {
             this.IsBusy = true;
         }
+        #endregion
 
+        #region 释放繁忙状态 —— void Idle()
         /// <summary>
         /// 释放繁忙状态
         /// </summary>
@@ -30,5 +112,100 @@ namespace SD.Infrastructure.WPF.Caliburn.Base
         {
             this.IsBusy = false;
         }
+        #endregion
+
+        #region 提示消息 —— void ToastInfo(string message...
+        /// <summary>
+        /// 提示消息
+        /// </summary>
+        /// <param name="message">消息</param>
+        /// <param name="onClick">点击事件</param>
+        /// <param name="onClose">关闭事件</param>
+        public void ToastInfo(string message, Action<NotificationBase> onClick = null, Action<NotificationBase> onClose = null)
+        {
+            MessageOptions options = new MessageOptions
+            {
+                FontSize = this.NotifierOptions.FontSize,
+                ShowCloseButton = this.NotifierOptions.ShowCloseButton,
+                FreezeOnMouseEnter = this.NotifierOptions.FreezeOnMouseEnter,
+                UnfreezeOnMouseLeave = this.NotifierOptions.UnfreezeOnMouseLeave,
+                NotificationClickAction = onClick,
+                CloseClickAction = onClose
+            };
+
+            this._notifier.ShowInformation(message, options);
+        }
+        #endregion
+
+        #region 提示成功 —— void ToastSuccess(string message...
+        /// <summary>
+        /// 提示成功
+        /// </summary>
+        /// <param name="message">消息</param>
+        /// <param name="onClick">点击事件</param>
+        /// <param name="onClose">关闭事件</param>
+        public void ToastSuccess(string message, Action<NotificationBase> onClick = null, Action<NotificationBase> onClose = null)
+        {
+            MessageOptions options = new MessageOptions
+            {
+                FontSize = this.NotifierOptions.FontSize,
+                ShowCloseButton = this.NotifierOptions.ShowCloseButton,
+                FreezeOnMouseEnter = this.NotifierOptions.FreezeOnMouseEnter,
+                UnfreezeOnMouseLeave = this.NotifierOptions.UnfreezeOnMouseLeave,
+                NotificationClickAction = onClick,
+                CloseClickAction = onClose
+            };
+
+            this._notifier.ShowSuccess(message, options);
+        }
+        #endregion
+
+        #region 提示警告 —— void ToastWarning(string message...
+        /// <summary>
+        /// 提示警告
+        /// </summary>
+        /// <param name="message">消息</param>
+        /// <param name="onClick">点击事件</param>
+        /// <param name="onClose">关闭事件</param>
+        public void ToastWarning(string message, Action<NotificationBase> onClick = null, Action<NotificationBase> onClose = null)
+        {
+            MessageOptions options = new MessageOptions
+            {
+                FontSize = this.NotifierOptions.FontSize,
+                ShowCloseButton = this.NotifierOptions.ShowCloseButton,
+                FreezeOnMouseEnter = this.NotifierOptions.FreezeOnMouseEnter,
+                UnfreezeOnMouseLeave = this.NotifierOptions.UnfreezeOnMouseLeave,
+                NotificationClickAction = onClick,
+                CloseClickAction = onClose
+            };
+
+            this._notifier.ShowWarning(message, options);
+        }
+        #endregion
+
+        #region 提示错误 —— void ToastError(string message...
+        /// <summary>
+        /// 提示错误
+        /// </summary>
+        /// <param name="message">消息</param>
+        /// <param name="onClick">点击事件</param>
+        /// <param name="onClose">关闭事件</param>
+        public void ToastError(string message, Action<NotificationBase> onClick = null, Action<NotificationBase> onClose = null)
+        {
+            MessageOptions options = new MessageOptions
+            {
+                FontSize = this.NotifierOptions.FontSize,
+                ShowCloseButton = this.NotifierOptions.ShowCloseButton,
+                FreezeOnMouseEnter = this.NotifierOptions.FreezeOnMouseEnter,
+                UnfreezeOnMouseLeave = this.NotifierOptions.UnfreezeOnMouseLeave,
+                NotificationClickAction = onClick,
+                CloseClickAction = onClose
+            };
+
+            this._notifier.ShowError(message, options);
+        }
+        #endregion
+
+        #endregion
     }
 }
---
+++ src/SD.Infrastructure.WPF.Caliburn/Models/NotifierOptions.cs
@@ -0,0 +1,108 @@
+﻿using ToastNotifications.Position;
+
+namespace SD.Infrastructure.WPF.Caliburn.Models
+{
+    /// <summary>
+    /// 通知设置
+    /// </summary>
+    public class NotifierOptions
+    {
+        #region # 构造器
+
+        #region 01.创建通知设置构造器
+        /// <summary>
+        /// 创建通知设置构造器
+        /// </summary>
+        /// <param name="location">位置</param>
+        /// <param name="marginX">水平边距</param>
+        /// <param name="marginY">垂直边距</param>
+        /// <param name="lifetime">生命周期（秒）</param>
+        /// <param name="maxCount">最大数量</param>
+        /// <param name="fontSize">字体大小</param>
+        /// <param name="showCloseButton">显示关闭按钮</param>
+        /// <param name="freezeOnMouseEnter">鼠标进入冻结</param>
+        /// <param name="unfreezeOnMouseLeave">鼠标离开取消冻结</param>
+        public NotifierOptions(Corner location = Corner.BottomCenter, double marginX = 25, double marginY = 25, int lifetime = 5, int maxCount = 10, double fontSize = 14, bool showCloseButton = true, bool freezeOnMouseEnter = true, bool unfreezeOnMouseLeave = true)
+        {
+            this.Location = location;
+            this.MarginX = marginX;
+            this.MarginY = marginY;
+            this.Lifetime = lifetime;
+            this.MaxCount = maxCount;
+            this.FontSize = fontSize;
+            this.ShowCloseButton = showCloseButton;
+            this.FreezeOnMouseEnter = freezeOnMouseEnter;
+            this.UnfreezeOnMouseLeave = unfreezeOnMouseLeave;
+        }
+        #endregion 
+
+        #endregion
+
+        #region # 属性
+
+        #region 位置 —— Corner Location
+        /// <summary>
+        /// 位置
+        /// </summary>
+        public Corner Location { get; set; }
+        #endregion
+
+        #region 水平边距 —— double MarginX
+        /// <summary>
+        /// 水平边距
+        /// </summary>
+        public double MarginX { get; set; }
+        #endregion
+
+        #region 垂直边距 —— double MarginY
+        /// <summary>
+        /// 垂直边距
+        /// </summary>
+        public double MarginY { get; set; }
+        #endregion
+
+        #region 生命周期（秒） —— int Lifetime
+        /// <summary>
+        /// 生命周期（秒）
+        /// </summary>
+        public int Lifetime { get; set; }
+        #endregion
+
+        #region 最大数量 —— int MaxCount
+        /// <summary>
+        /// 最大数量
+        /// </summary>
+        public int MaxCount { get; set; }
+        #endregion
+
+        #region 字体大小 —— double FontSize
+        /// <summary>
+        /// 字体大小
+        /// </summary>
+        public double FontSize { get; set; }
+        #endregion
+
+        #region 显示关闭按钮 —— bool ShowCloseButton
+        /// <summary>
+        /// 显示关闭按钮
+        /// </summary>
+        public bool ShowCloseButton { get; set; }
+        #endregion
+
+        #region 鼠标进入冻结 —— bool FreezeOnMouseEnter
+        /// <summary>
+        /// 鼠标进入冻结
+        /// </summary>
+        public bool FreezeOnMouseEnter { get; set; }
+        #endregion
+
+        #region 鼠标离开取消冻结 —— bool UnfreezeOnMouseLeave
+        /// <summary>
+        /// 鼠标离开取消冻结
+        /// </summary>
+        public bool UnfreezeOnMouseLeave { get; set; }
+        #endregion
+
+        #endregion
+    }
+}
