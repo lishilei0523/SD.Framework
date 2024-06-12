2d1fcad078293d92602b11cc18fc89d1b48b1c8a
Lee <lishilei0523@163.com>
2024-06-12 13:04:37 +0800

    WPF增加可缩放Canvas、2D网格控件；

---
+++ src/SD.Infrastructure.WPF/CustomControls/GridLinesVisual2D.cs
@@ -0,0 +1,142 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace SD.Infrastructure.WPF.CustomControls
+{
+    /// <summary>
+    /// 2D网格线
+    /// </summary>
+    public class GridLinesVisual2D : Shape
+    {
+        #region # 字段及构造器
+
+        /// <summary>
+        /// 行数依赖属性
+        /// </summary>
+        public static readonly DependencyProperty RowsProperty;
+
+        /// <summary>
+        /// 列数依赖属性
+        /// </summary>
+        public static readonly DependencyProperty ColsProperty;
+
+        /// <summary>
+        /// 步长依赖属性
+        /// </summary>
+        public static readonly DependencyProperty StepSizeProperty;
+
+        /// <summary>
+        /// 静态构造器
+        /// </summary>
+        static GridLinesVisual2D()
+        {
+            RowsProperty = DependencyProperty.Register(nameof(Rows), typeof(int), typeof(GridLinesVisual2D), new PropertyMetadata(2000));
+            ColsProperty = DependencyProperty.Register(nameof(Cols), typeof(int), typeof(GridLinesVisual2D), new PropertyMetadata(2000));
+            StepSizeProperty = DependencyProperty.Register(nameof(StepSize), typeof(int), typeof(GridLinesVisual2D), new PropertyMetadata(100));
+        }
+
+        /// <summary>
+        /// 默认构造器
+        /// </summary>
+        public GridLinesVisual2D()
+        {
+            base.Stroke = new SolidColorBrush(Colors.Gray);
+            Panel.SetZIndex(this, int.MaxValue);
+        }
+
+        #endregion
+
+        #region # 属性
+
+        #region 依赖属性 - 行数 —— int Rows
+        /// <summary>
+        /// 依赖属性 - 行数
+        /// </summary>
+        public int Rows
+        {
+            get => (int)this.GetValue(RowsProperty);
+            set => this.SetValue(RowsProperty, value);
+        }
+        #endregion
+
+        #region 依赖属性 - 列数 —— int Cols
+        /// <summary>
+        /// 依赖属性 - 列数
+        /// </summary>
+        public int Cols
+        {
+            get => (int)this.GetValue(ColsProperty);
+            set => this.SetValue(ColsProperty, value);
+        }
+        #endregion
+
+        #region 依赖属性 - 步长 —— int StepSize
+        /// <summary>
+        /// 依赖属性 - 步长
+        /// </summary>
+        public int StepSize
+        {
+            get => (int)this.GetValue(StepSizeProperty);
+            set => this.SetValue(StepSizeProperty, value);
+        }
+        #endregion
+
+        #region 只读属性 - 几何对象 —— override Geometry DefiningGeometry
+        /// <summary>
+        /// 只读属性 - 几何对象
+        /// </summary>
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                GeometryGroup gridLines = new GeometryGroup();
+                for (int x = 0; x <= this.Rows; x += this.StepSize)
+                {
+                    LineGeometry verticalLine = new LineGeometry
+                    {
+                        StartPoint = new Point(x, 0),
+                        EndPoint = new Point(x, this.Cols)
+                    };
+                    gridLines.Children.Add(verticalLine);
+                }
+                for (int y = 0; y <= this.Cols; y += this.StepSize)
+                {
+                    LineGeometry horizontalLine = new LineGeometry
+                    {
+                        StartPoint = new Point(0, y),
+                        EndPoint = new Point(this.Rows, y)
+                    };
+                    gridLines.Children.Add(horizontalLine);
+                }
+
+                return gridLines;
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region # 方法
+
+        #region 渲染事件 —— override void OnRender(DrawingContext drawingContext)
+        /// <summary>
+        ///  渲染事件
+        /// </summary>
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            //绘制中心点
+            Point center = new Point(this.Cols / 2.0, this.Rows / 2.0);
+            double thickness = base.StrokeThickness * 2;
+            SolidColorBrush brush = new SolidColorBrush(Colors.Red);
+            Pen pen = new Pen(brush, thickness);
+            drawingContext.DrawEllipse(brush, pen, center, thickness, thickness);
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ src/SD.Infrastructure.WPF/CustomControls/ScalableCanvas.cs
@@ -0,0 +1,203 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace SD.Infrastructure.WPF.CustomControls
+{
+    /// <summary>
+    /// 可缩放Canvas
+    /// </summary>
+    public class ScalableCanvas : Canvas
+    {
+        #region # 字段及构造器
+
+        /// <summary>
+        /// 缩放系数依赖属性
+        /// </summary>
+        public static readonly DependencyProperty ScaledFactorProperty;
+
+        /// <summary>
+        /// 静态构造器
+        /// </summary>
+        static ScalableCanvas()
+        {
+            ScaledFactorProperty = DependencyProperty.Register(nameof(ScaledFactor), typeof(float), typeof(ScalableCanvas), new PropertyMetadata(1.1f));
+        }
+
+        /// <summary>
+        /// 矫正起始位置
+        /// </summary>
+        private Point? _rectifiedStartPosition;
+
+        /// <summary>
+        /// 变换矩阵
+        /// </summary>
+        private readonly MatrixTransform _matrixTransform;
+
+        /// <summary>
+        /// 默认构造器
+        /// </summary>
+        public ScalableCanvas()
+        {
+            this._matrixTransform = new MatrixTransform();
+            base.MouseDown += this.OnMouseDown;
+            base.MouseMove += this.OnMouseMove;
+            base.MouseWheel += this.OnMouseWheel;
+            base.MouseUp += this.OnMouseUp;
+            base.HorizontalAlignment = HorizontalAlignment.Stretch;
+            base.VerticalAlignment = VerticalAlignment.Stretch;
+            base.Background = new SolidColorBrush(Colors.Transparent);
+        }
+        #endregion
+
+        #region # 属性
+
+        #region 依赖属性 - 缩放系数 —— float ScaledFactor
+        /// <summary>
+        /// 依赖属性 - 缩放系数
+        /// </summary>
+        public float ScaledFactor
+        {
+            get => (float)this.GetValue(ScaledFactorProperty);
+            set => this.SetValue(ScaledFactorProperty, value);
+        }
+        #endregion
+
+        #region 只读属性 - 缩放率 —— double ScaledRatio
+        /// <summary>
+        /// 只读属性 - 缩放率
+        /// </summary>
+        public double ScaledRatio
+        {
+            get => this._matrixTransform.Matrix.M11;
+        }
+        #endregion
+
+        #region 只读属性 - 变换矩阵 —— MatrixTransform MatrixTransform
+        /// <summary>
+        /// 只读属性 - 变换矩阵
+        /// </summary>
+        public MatrixTransform MatrixTransform
+        {
+            get => this._matrixTransform;
+        }
+        #endregion
+
+        #endregion
+
+        #region # 方法
+
+        #region 获取矫正左边距 —— double GetRectifiedLeft(UIElement element)
+        /// <summary>
+        /// 获取矫正左边距
+        /// </summary>
+        /// <param name="element">UI元素</param>
+        /// <returns>矫正左边距</returns>
+        public double GetRectifiedLeft(UIElement element)
+        {
+            double left = GetLeft(element);
+            double retifiedLeft = left / this.ScaledRatio;
+
+            return retifiedLeft;
+        }
+        #endregion
+
+        #region 获取矫正上边距 —— double GetRectifiedTop(UIElement element)
+        /// <summary>
+        /// 获取矫正上边距
+        /// </summary>
+        /// <param name="element">UI元素</param>
+        /// <returns>矫正上边距</returns>
+        public double GetRectifiedTop(UIElement element)
+        {
+            double top = GetTop(element);
+            double retifiedTop = top / this.ScaledRatio;
+
+            return retifiedTop;
+        }
+        #endregion
+
+        #region 鼠标按下事件 —— void OnMouseDown(object sender, MouseButtonEventArgs eventArgs)
+        /// <summary>
+        /// 鼠标按下事件
+        /// </summary>
+        private void OnMouseDown(object sender, MouseButtonEventArgs eventArgs)
+        {
+            if (eventArgs.ChangedButton == MouseButton.Middle)
+            {
+                Point mousePosition = eventArgs.GetPosition(this);
+                this._rectifiedStartPosition = this._matrixTransform.Inverse!.Transform(mousePosition);
+            }
+        }
+        #endregion
+
+        #region 鼠标移动事件 —— void OnMouseMove(object sender, MouseEventArgs eventArgs)
+        /// <summary>
+        /// 鼠标移动事件
+        /// </summary>
+        private void OnMouseMove(object sender, MouseEventArgs eventArgs)
+        {
+            if (eventArgs.MiddleButton == MouseButtonState.Pressed && this._rectifiedStartPosition.HasValue)
+            {
+                //设置光标
+                Mouse.OverrideCursor = Cursors.Hand;
+
+                Point mousePosition = eventArgs.GetPosition(this);
+                Point mousePositionInverse = this._matrixTransform.Inverse!.Transform(mousePosition);
+                Vector delta = Point.Subtract(mousePositionInverse, this._rectifiedStartPosition.Value);
+                TranslateTransform transform = new TranslateTransform(delta.X, delta.Y);
+                this._matrixTransform.Matrix = transform.Value * this._matrixTransform.Matrix;
+                foreach (UIElement element in this.Children)
+                {
+                    element.RenderTransform = this._matrixTransform;
+                }
+            }
+        }
+        #endregion
+
+        #region 鼠标滚轮事件 —— void OnMouseWheel(object sender, MouseWheelEventArgs eventArgs)
+        /// <summary>
+        /// 鼠标滚轮事件
+        /// </summary>
+        private void OnMouseWheel(object sender, MouseWheelEventArgs eventArgs)
+        {
+            float scaledFactor = this.ScaledFactor;
+            if (eventArgs.Delta < 0)
+            {
+                scaledFactor = 1f / scaledFactor;
+            }
+
+            Point mousePostion = eventArgs.GetPosition(this);
+            Matrix scaledMatrix = this._matrixTransform.Matrix;
+            scaledMatrix.ScaleAt(scaledFactor, scaledFactor, mousePostion.X, mousePostion.Y);
+            this._matrixTransform.Matrix = scaledMatrix;
+            foreach (UIElement element in this.Children)
+            {
+                double x = GetLeft(element);
+                double y = GetTop(element);
+                double scaledX = x * scaledFactor;
+                double scaledY = y * scaledFactor;
+                SetLeft(element, scaledX);
+                SetTop(element, scaledY);
+                element.RenderTransform = this._matrixTransform;
+            }
+        }
+        #endregion
+
+        #region 鼠标松开事件 —— void OnMouseUp(object sender, MouseButtonEventArgs eventArgs)
+        /// <summary>
+        /// 鼠标松开事件
+        /// </summary>
+        private void OnMouseUp(object sender, MouseButtonEventArgs eventArgs)
+        {
+            //设置光标
+            Mouse.OverrideCursor = Cursors.Arrow;
+
+            this._rectifiedStartPosition = null;
+        }
+        #endregion 
+
+        #endregion
+    }
+}
