71bccde5063d2be57026bc6bedee8f80699a8be1
Lee <lishilei0523@163.com>
2026-04-25 21:07:18 +0800

    添加ToggleRadioButton控件；

---
+++ src/SD.Infrastructure.Avalonia/CustomControls/ToggleRadioButton.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using System;
+
+namespace SD.Infrastructure.Avalonia.CustomControls
+{
+    /// <summary>
+    /// Toggle形式RadioButton
+    /// </summary>
+    public class ToggleRadioButton : RadioButton
+    {
+        /// <summary>
+        /// 样式键覆盖
+        /// </summary>
+        protected override Type StyleKeyOverride
+        {
+            get => typeof(ToggleButton);
+        }
+    }
+}
