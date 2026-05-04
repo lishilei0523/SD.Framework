4f7b79e13d7d0c761be3cfeb5ace9e7a83db055c
Lee <lishilei0523@163.com>
2026-05-04 17:31:51 +0800

    添加Caliburn事件基类；

---
+++ src/SD.Infrastructure.Avalonia.Caliburn/Base/CaliburnEvent.cs
@@ -0,0 +1,15 @@
+﻿using Caliburn.Micro;
+
+namespace SD.Infrastructure.Avalonia.Caliburn.Base
+{
+    /// <summary>
+    /// Caliburn事件
+    /// </summary>
+    public abstract class CaliburnEvent
+    {
+        /// <summary>
+        /// 事件发布者
+        /// </summary>
+        public Screen Publisher { get; set; }
+    }
+}
